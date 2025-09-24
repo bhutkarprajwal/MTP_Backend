cfafc3d9cb932f296e9fea23dd2e902ae1c04c8c
bhutkarprajwal <140779644+bhutkarprajwal@users.noreply.github.com>
2025-09-24 22:11:34 +0530

    updated to server

--- MTDC_BACKEND/local_settings.py
+++ MTDC_BACKEND/local_settings.py
@@ -1,27 +1,27 @@
-# from .settings import *
+from .settings import *
 
-# DEBUG = True
-# ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+DEBUG = True
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
-# # Local MySQL database (or you can use SQLite)
+# Local MySQL database (or you can use SQLite)
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mtdc_db',
+        'USER': 'root',
+        'PASSWORD': 'Prajwal@1234',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
+    }
+}
+
+# Or use SQLite instead for simpler setup:
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'mtdc_db',
-#         'USER': 'root',
-#         'PASSWORD': 'Prajwal@1234',
-#         'HOST': 'localhost',
-#         'PORT': '3306',
-#         'OPTIONS': {
-#             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-#         },
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
-
-# # Or use SQLite instead for simpler setup:
-# # DATABASES = {
-# #     'default': {
-# #         'ENGINE': 'django.db.backends.sqlite3',
-# #         'NAME': BASE_DIR / 'db.sqlite3',
-# #     }
-# # }
